006cc6a781a36f1fe8d7eaf20f8c7edcf7f164b9
tomi22 <tomasmiguelf94@gmail.com>
2016-09-09 20:16:21 -0300

    9/9
    Paquetes y beta uml

---
+++ CiudadDeBatalla/src/Proyectil/Proyectil.java
@@ -0,0 +1,26 @@
+package Proyectil;
+import general.Celda;
+import general.Coordenada;
+
+public class Proyectil extends Celda {
+	
+	public Celda afectar(){
+		
+		
+	}
+	
+	
+	@Override
+	public Coordenada ubicacion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Coordenada getUbicacion() {
+		
+		return null;
+	}
+	
+	
+}
